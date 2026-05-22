77e93467a00330b4dbf7907b60de3b1522eee01d
Nimrat Kaur <nimrat21randhawa@gmail.com>
2026-05-22 15:34:25 +0530

    Time: 0 ms (100%), Space: 8.6 MB (12.87%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        int s=0;
+        int e=x;
+        long long int mid=s+(e-s)/2;
+        int ans=-1;
+
+        while(s<=e) {
+            if(x==(mid*mid)) {
+                return mid;
+            }
+            else if(x<(mid*mid)) {
+                e=mid-1;
+            }
+            else {
+                ans=mid;
+                s=mid+1;
+            }
+            mid=s+(e-s)/2;
+        }
+
+        return ans;
+        
+    }
+};
