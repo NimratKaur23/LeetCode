882d7ac044e55e32fa9e60454751d9ab2f88e954
Nimrat Kaur <nimrat21randhawa@gmail.com>
2026-07-15 16:37:54 +0530

    Time: 0 ms (100%), Space: 8.6 MB (46.67%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    int gcdOfOddEvenSums(int n) {
+        // int sumOdd=0;
+        // int sumEven=0;
+
+        // int count=n;
+        // int i=1;
+        // while(2*count) {
+        //     if(i%2!=0)
+        //      sumOdd=sumOdd+i;
+        //     else
+        //      sumEven=sumEven+i;
+        
+        // count--;
+        // i++;
+        // }
+
+        return n;
+
+    }
+};
