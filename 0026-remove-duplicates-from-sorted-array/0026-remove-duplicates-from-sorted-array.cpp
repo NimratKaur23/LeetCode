78e44237baace41a0cff57b7dd64a84a6d66f3dc
Nimrat Kaur <nimrat21randhawa@gmail.com>
2026-08-31 19:11:46 +0530

    Time: 0 ms (100%), Space: 22.6 MB (80.68%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -2,21 +2,17 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
 
-        if(nums.size()==0) {
-            return 0;
-        }
+        if(nums.size()==0)
+          return 0;
 
-        int k=1;
-
+        int count=1;
         for(int i=1;i<nums.size();i++) {
             if(nums[i]!=nums[i-1]) {
-                nums[k]=nums[i];
-                k++;
+                nums[count]=nums[i];
+                count++;
             }
         }
 
-        return k;
-
-        
+        return count;
     }
 };
