d43559ae99820c5ab241c5cffefc1b38449551cb
Nimrat Kaur <nimrat21randhawa@gmail.com>
2026-09-08 19:29:53 +0530

    Time: 0 ms (100%), Space: 7.7 MB (97%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+int sq(int n) {
+    int sum=0;
+    while(n>0) {
+        int rem=n%10;
+        sum=sum+rem*rem;
+        n=n/10;
+    }
+
+    return sum;
+}
+
+    bool isHappy(int n) {
+        int slow=n;
+        int fast=n;
+
+        while(fast!=1) {
+            slow=sq(slow);
+            fast=sq(fast);
+            fast=sq(fast);
+
+            if(slow==fast && slow!=1)
+              return false;
+        }
+
+        return true;
+    }
+};
