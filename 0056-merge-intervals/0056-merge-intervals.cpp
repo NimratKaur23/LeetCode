419cd192b3ee8600adbe7afe5d08b1b377ff31e7
Nimrat Kaur <nimrat21randhawa@gmail.com>
2026-09-16 15:40:53 +0530

    Time: 1 ms (94.97%), Space: 23.9 MB (60.43%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,56 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-
-
-       /* 
         vector<vector<int>> ans;
-
-        for(int i=0;i<intervals.size();i=i+2) {
-            for(int j=i+1;j<intervals.size();j++) {
-                if(intervals[i][1]==intervals[j][0] || intervals[i][1]-1==intervals[j][0]) {
-                    vector<int> temp;
-                    temp.push_back(intervals[i][0]);
-                    temp.push_back(intervals[j][1]);
-                    ans.push_back(temp);
-                }
-                else if (intervals[i][0]==intervals[j][1]) {
-                    vector<int> temp;
-                    temp.push_back(intervals[j][0]);
-                    temp.push_back(intervals[i][1]);
-                    ans.push_back(temp);
-                }
-                else {
-                   ans.push_back(intervals[i]);
-                   ans.push_back(intervals[j]);
-                }
-            }
-        }
-
-        return ans;
-        */
 
         sort(intervals.begin(),intervals.end());
-
-        vector<vector<int>> ans;
-        vector<int> prev=intervals[0];
+        
+        int start1=intervals[0][0];
+        int end1=intervals[0][1];
 
         for(int i=1;i<intervals.size();i++) {
-            if(intervals[i][0]<=prev[1]) {
-                prev[1]=max(prev[1],intervals[i][1]);
+            int start2=intervals[i][0];
+            int end2=intervals[i][1];
+
+            if(end1>=start2) {
+                //do merge
+                //new range
+                start1=start1;
+                end1=max(end1,end2);
+                continue;
             }
-            else {
-                ans.push_back(prev);
-                prev=intervals[i];
-            }
+
+            ans.push_back({start1,end1});
+            //update start1 and end 1
+            start1=start2;
+            end1=end2;
         }
 
-        ans.push_back(prev);
+        ans.push_back({start1,end1});
+
         return ans;
-
-
-
-
     }
-
 };
