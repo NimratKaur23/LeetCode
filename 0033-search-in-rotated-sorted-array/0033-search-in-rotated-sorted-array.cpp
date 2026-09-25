7790d30d1d9fe48b3ecdc6d534052d3b224aceb8
Nimrat Kaur <nimrat21randhawa@gmail.com>
2026-09-25 12:31:01 +0530

    Time: 0 ms (100%), Space: 15.3 MB (37.64%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,40 +1,51 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-
         int s=0;
         int e=nums.size()-1;
-        int mid=s+(e-s)/2;
+        int n=nums.size();
 
         while(s<=e) {
+            int mid=(e+s)/2;
+
             if(nums[mid]==target) {
                 return mid;
             }
-           
-           if(nums[s]<=nums[mid]) {
-            if(target>=nums[s] && target<=nums[mid]) {
-                e=mid-1;
+
+            //is mid in part1
+            if(nums[mid]>nums[n-1]) {
+                if(nums[mid]<target) {
+                  s=mid+1;
+                }
+                else{
+                    if(nums[0]>target) {
+                        //go right
+                        s=mid+1;
+                    }
+                    else {
+                        e=mid-1;
+                    }
+                }
             }
-            else {
-                s=mid+1;
-            }
-           }
+            //part2 mid lie there
 
             else {
-                if(target>=nums[mid] && target<=nums[e]) {
-                    s=mid+1;
-                }
-                else {
+                if(nums[mid]>target) {
                     e=mid-1;
                 }
-            }
-           
-         
+                else {
+                    if(nums[n-1]<target)
+                      e=mid-1;
+                    else
+                      s=mid+1;
+                }
 
-           mid=s+(e-s)/2;
+            }
+
+
+
         }
 
         return -1;
-        
     }
 };
